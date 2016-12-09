f57f291ddd5909c4491b297db9aebe2b5f036772
ashok9748 <thakurashokkumar350@gmail.com>
2016-12-09 19:20:09 +0530

    PrintName
    Printname without semicolon

---
+++ PrintName/src/com/PrintName.java
@@ -0,0 +1,11 @@
+package com;
+
+public class PrintName {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        if (System.out.printf("Ashok Kumar Thakur\n") == null) {}
+	}
+
+}
